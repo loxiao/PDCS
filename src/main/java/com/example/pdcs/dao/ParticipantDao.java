f8d127e8fbb7639c6378f16b443c32a7e1ad44cf
usx.22145407 <2972204889@qq.com>
2024-07-10 16:00:26 +0800

    顾煜修改了sql语句

--- src/main/java/com/example/pdcs/dao/ParticipantDao.java
+++ src/main/java/com/example/pdcs/dao/ParticipantDao.java
@@ -1,8 +1,15 @@
 package com.example.pdcs.dao;
 
+import com.example.pdcs.domain.Participant;
 import com.example.pdcs.util.JDBCUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ParticipantDao {
     private JdbcTemplate template = new JdbcTemplate(JDBCUtils.getDataSource());
+//    public Participant getbynumberandpwd(String Number,String pwd){
+//        Participant participant=null;
+//        try{
+//            String sql=""
+//        }
+//    }
 }
