5504b27112ba8ea0e4c84f3480f1a41977d13f0e
USXCJL <953882278@qq.com>
2024-07-16 10:38:46 +0800

    更新了个人中心的我要报名界面和根据TypeId查询未截止报名的竞赛（陈俊龙）

--- src/main/java/com/example/pdcs/servlet/CompetitionsListServlet.java
+++ src/main/java/com/example/pdcs/servlet/CompetitionsListServlet.java
@@ -1,8 +1,10 @@
 package com.example.pdcs.servlet;
 
 import com.example.pdcs.dao.CompetitionsDao;
+import com.example.pdcs.dao.CompetitiontypesDao;
 import com.example.pdcs.dao.WorksDao;
 import com.example.pdcs.domain.Competitions;
+import com.example.pdcs.domain.Competitiontypes;
 import com.example.pdcs.domain.Works;
 
 import javax.servlet.*;
@@ -16,8 +18,18 @@ public class CompetitionsListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         CompetitionsDao competitionsDao = new CompetitionsDao();
-        List<Competitions> competitionsList = competitionsDao.getnewcompetitions();
-        request.getSession().setAttribute("competitionsList",competitionsList);
+        CompetitiontypesDao competitiontypesDao = new CompetitiontypesDao();
+        String Typeid = request.getParameter("TypeId");
+        if (Typeid != null){
+            int id = Integer.parseInt(Typeid);
+            List<Competitions> competitionsList = competitionsDao.getCompetitionsByTypeOrAll(id);
+            request.getSession().setAttribute("competitionsList",competitionsList);
+        }else {
+            List<Competitions> competitionsList = competitionsDao.getCompetitionsByTypeOrAll(0);
+            request.getSession().setAttribute("competitionsList",competitionsList);
+        }
+        List<Competitiontypes> competitiontypes = competitiontypesDao.getList();
+        request.getSession().setAttribute("Types",competitiontypes);
         request.getRequestDispatcher("myEntry.jsp").forward(request, response);
     }
     @Override
